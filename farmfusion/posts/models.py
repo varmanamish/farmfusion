5a73ce2fc9fbd383fbbab205ce4b7aa5a96641fb
Revanth Sallangula <theblazecode@gmail.com>
2025-02-16 22:36:09 +0530

    sum

--- farmfusion/posts/models.py
+++ farmfusion/posts/models.py
@@ -4,7 +4,7 @@ class Post(models.Model):
     username = models.CharField(max_length=255)
     location = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    profile_pic = models.ImageField(upload_to='profile_pics/', default='profile_pics/default.jpg')
+    profile_pic = models.ImageField(upload_to='profile_pics/')
     post_image = models.ImageField(upload_to='post_images/')
     caption = models.TextField(blank=True)
     likes_count = models.IntegerField(default=0)
