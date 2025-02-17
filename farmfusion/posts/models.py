95742e90c44e176d8bde23a658412117cc511935
Revanth Sallangula <theblazecode@gmail.com>
2025-02-17 13:01:40 +0530

    dump username

--- farmfusion/posts/models.py
+++ farmfusion/posts/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from accounts.models import CustomUser  # Import the CustomUser model
 
-class postdetails(models.Model):
-    username = models.CharField(max_length=255)
+class PostDetails(models.Model):
+    user = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
+    username = models.CharField(max_length=255, default='Anonymous')  # Ensure default value is set
     location = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     profile_pic = models.ImageField(upload_to='profile_pics/')
     post_image = models.ImageField(upload_to='post_images/')
     caption = models.TextField(blank=True)
     likes_count = models.IntegerField(default=0)
+
--- farmfusion/posts/views.py
+++ farmfusion/posts/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
-from .models import postdetails as Post
+from .models import PostDetails as Post
 
 def feed(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'posts/feed.html', {'posts': posts})
 
+
+# @login_required  # Ensure only logged-in users can post
 def add_post(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
         location = request.POST.get('location', '')  
         caption = request.POST.get('caption', '')
         profile_pic = request.FILES.get('profile_pic')
@@ -15,13 +16,13 @@ def add_post(request):
 
         # Ensure files are uploaded properly
         if profile_pic:
-            profile_pic.name = f"profile_{username}_{profile_pic.name}"
+            profile_pic.name = f"profile_{request.user.username}_{profile_pic.name}"
         if post_image:
-            post_image.name = f"post_{username}_{post_image.name}"
+            post_image.name = f"post_{request.user.username}_{post_image.name}"
 
         # Create and save the post
         Post.objects.create(
-            username=username,
+            user=request.user,  # Automatically assign the logged-in user
             location=location,
             caption=caption,
             profile_pic=profile_pic,
@@ -30,3 +31,5 @@ def add_post(request):
         )
 
         return redirect('feed')  # Redirect to the feed page after posting
+
+    return render(request, 'posts/add_post.html')  # Render form if GET request
