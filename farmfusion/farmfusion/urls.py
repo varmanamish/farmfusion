1a307d9c44cdfa243ce413f15cfdde626bd18282
Revanth Sallangula <theblazecode@gmail.com>
2025-02-19 18:12:11 +0530

    create shop page

--- farmfusion/farmfusion/urls.py
+++ farmfusion/farmfusion/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('ecom.urls')),
     path('posts/', include("posts.urls")),
+    path('shop/', include('ecommerce.urls')),
     path('accounts/',include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
