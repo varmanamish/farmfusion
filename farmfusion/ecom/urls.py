f31b21bc41b05f3596d3f2344fabcd6402e02ca5
Preetham Janugam <janugampreetham17@gmail.com>
2025-02-22 09:06:53 +0530

    last to final

--- farmfusion/ecom/urls.py
+++ farmfusion/ecom/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-
+from ecommerce.views import shop
+from posts.views import feed
 urlpatterns=[
     path('',views.index,name="index"),
-    path('products', views.products, name="products"),
-    path('services', views.services, name="services"),
+    path('feed', feed, name="feed"),
+    path('shop', shop, name="shop"),
     path('contacts', views.contacts, name="contacts"),
     path('partners', views.partners, name="partners"),
     path('privacy', views.privacy, name="privacy"),
