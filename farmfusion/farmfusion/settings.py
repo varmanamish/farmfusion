9ed4df30da0c3105e37f76f98705243bc35f4aec
vamshikrishna630 <vamshibura2004@gmail.com>
2025-04-01 22:29:24 +0530

    done

--- farmfusion/farmfusion/settings.py
+++ farmfusion/farmfusion/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'farmfusion',
         'USER':'postgres',
-        'PASSWORD':'1234',
+        'PASSWORD':'12345',
         'HOST':'localhost',
     }
 }
